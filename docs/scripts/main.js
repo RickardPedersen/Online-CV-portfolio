439b50650782c015df12073ba4d6fe5745a2d9e4
RickardPedersen <54976141+RickardPedersen@users.noreply.github.com>
2019-11-14 19:08:38 +0100

    Added functionality to navbar links

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,7 +1,43 @@
 $(function() {
-    $(".nav-link").on("click", function() {
+    //Navbar links
+
+    //Landing
+    $("#landing-button").on("click", function() {
         $(".active").removeClass("active");
         $(this).addClass("active");
+        $(".pages").addClass("d-none");
+        $("#landing").removeClass("d-none");
+    });
+
+    //About
+    $("#about-button").on("click", function() {
+        $(".active").removeClass("active");
+        $(this).addClass("active");
+        $(".pages").addClass("d-none");
+        $("#about").removeClass("d-none");
+    });
+
+    //Education
+    $("#education-button").on("click", function() {
+        $(".active").removeClass("active");
+        $(this).addClass("active");
+        $(".pages").addClass("d-none");
+        $("#education").removeClass("d-none");
+    });
+
+    //Work
+    $("#work-button").on("click", function() {
+        $(".active").removeClass("active");
+        $(this).addClass("active");
+        $(".pages").addClass("d-none");
+        $("#work").removeClass("d-none");
+    });
+
+    //Portfolio
+    $("#portfolio-button").on("click", function() {
+        $(".active").removeClass("active");
+        $(this).addClass("active");
+        $(".pages").addClass("d-none");
         $("#portfolio").removeClass("d-none");
     });
 });
