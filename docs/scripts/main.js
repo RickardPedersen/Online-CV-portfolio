052850d845bf426bfdbb3fc7ee7d5933a7286515
RickardPedersen <54976141+RickardPedersen@users.noreply.github.com>
2019-11-17 20:13:19 +0100

    Ready for final testing

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,5 +1,5 @@
+// Checks if user has any language option saved
 let language = localStorage.getItem('language');
-
 if (language == null) {
     language = "sv";
 }
@@ -11,10 +11,13 @@ $(function () {
         $(".active").removeClass("active");
         $(this).addClass("active");
         $(".pages").addClass("d-none");
+        // I use the same key words in id:s for the navbar buttons and the
+        // pages so that this eventlistener can be used for all navbar buttons.
+        // education-button, education-page etc.
         $("#" + this.id.split("-")[0] + "-page").removeClass("d-none");
     });
 
-    // Checks users language option in localstorage 
+    // Checks users saved language option
     if (language == "en") {
         $('body [lang="sv"]').addClass("d-none");
         $('body [lang="en"]').removeClass("d-none");
@@ -22,16 +25,20 @@ $(function () {
 
     // Event listener for sv-button
     $("#sv-button").on("click", function () {
+        // Saves language option in local storage
         language = this.value;
         localStorage.setItem("language", language);
+        // Hides all elements with lang="en" attribute shows lang="sv" elements
         $('body [lang="en"]').addClass("d-none");
         $('body [lang="sv"]').removeClass("d-none");
     })
 
     // Event listener for en-button
     $("#en-button").on("click", function () {
+        // Saves language option in local storage
         language = this.value;
         localStorage.setItem("language", language);
+        // Hides all elements with lang="sv" attribute shows lang="en" elements
         $('body [lang="sv"]').addClass("d-none");
         $('body [lang="en"]').removeClass("d-none");
     })
