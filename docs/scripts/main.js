88252509f4163201c26b0ff55054f299354d67b7
RickardPedersen <54976141+RickardPedersen@users.noreply.github.com>
2019-11-17 03:01:18 +0100

    Did more stuff

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,12 +1,37 @@
+let language = localStorage.getItem('language');
+
+if (language == null) {
+    language = "sv";
+}
+
 $(function () {
     /*Navbar*/
     //Adds eventlisteners for navigation in the navbar
-    $(".navbar-brand, .nav-link").on("click", function () {
+    $(".navbar-brand, .navbar-nav > .nav-link").on("click", function () {
         $(".active").removeClass("active");
         $(this).addClass("active");
         $(".pages").addClass("d-none");
         $("#" + this.id.split("-")[0] + "-page").removeClass("d-none");
     });
+
+    if (language == "en") {
+        $('body [lang="sv"]').addClass("d-none");
+        $('body [lang="en"]').removeClass("d-none");
+    }
+
+    $("#sv-button").on("click", function () {
+        language = this.value;
+        localStorage.setItem("language", language);
+        $('body [lang="en"]').addClass("d-none");
+        $('body [lang="sv"]').removeClass("d-none");
+    })
+
+    $("#en-button").on("click", function () {
+        language = this.value;
+        localStorage.setItem("language", language);
+        $('body [lang="sv"]').addClass("d-none");
+        $('body [lang="en"]').removeClass("d-none");
+    })
     /*End of navbar*/
 
     /*Landing page*/
