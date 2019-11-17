874d636b181fe51426a85a5294906c3ada27612f
RickardPedersen <54976141+RickardPedersen@users.noreply.github.com>
2019-11-17 16:27:55 +0100

    Comments and indentation

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -6,7 +6,7 @@ if (language == null) {
 
 $(function () {
     /*Navbar*/
-    //Adds eventlisteners for navigation in the navbar
+    // Adds eventlisteners for navigation in the navbar
     $(".navbar-brand, .navbar-nav > .nav-link").on("click", function () {
         $(".active").removeClass("active");
         $(this).addClass("active");
@@ -14,11 +14,13 @@ $(function () {
         $("#" + this.id.split("-")[0] + "-page").removeClass("d-none");
     });
 
+    // Checks users language option in localstorage 
     if (language == "en") {
         $('body [lang="sv"]').addClass("d-none");
         $('body [lang="en"]').removeClass("d-none");
     }
 
+    // Event listener for sv-button
     $("#sv-button").on("click", function () {
         language = this.value;
         localStorage.setItem("language", language);
@@ -26,30 +28,20 @@ $(function () {
         $('body [lang="sv"]').removeClass("d-none");
     })
 
+    // Event listener for en-button
     $("#en-button").on("click", function () {
         language = this.value;
         localStorage.setItem("language", language);
         $('body [lang="sv"]').addClass("d-none");
         $('body [lang="en"]').removeClass("d-none");
     })
-    /*End of navbar*/
-
-    /*Landing page*/
-    /*End of landing page*/
-
-    /*About page page*/
-    /*End of about page*/
 
     /*Education page*/
-    //Accordion event listeners
+    // Accordion event listeners
     $(".accordion .card").on("click", function () {
-        //Slide down/up effect
+        // Slide down/up effect
         $(this).children(".collapse").slideToggle(250);
-        //Flips the arrow icon
+        // Flips the arrow icon
         $(this).find("img").toggleClass("flipImage");
     });
-    /*End of education page*/
-
-    /*Work page*/
-    /*End of work page*/
 });
